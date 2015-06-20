62052cafab4c49d6096846e41082887e231d3ac1
Eric Fischer <enf@pobox.com>
2015-06-19 17:29:56 -0700

    Move zoom traversal into C++

--- tile.h
+++ tile.h
@@ -27,5 +27,7 @@ struct pool_val *deserialize_string(char **f, struct pool *p, int type);
 
 long long write_tile(char **geom, char *metabase, char *stringpool, unsigned *file_bbox, int z, unsigned x, unsigned y, int detail, int min_detail, int basezoom, struct pool **file_keys, char **layernames, sqlite3 *outdb, double droprate, int buffer, const char *fname, FILE *geomfile[4], int file_minzoom, int file_maxzoom, double todo, char *geomstart, long long along, double gamma, int nlayers, char *prevent);
 
+int traverse_zooms(int geomfd[4], off_t geom_size[4], char *metabase, char *stringpool, unsigned *file_bbox, struct pool **file_keys, unsigned *midx, unsigned *midy, char **layernames, int maxzoom, int minzoom, sqlite3 *outdb, double droprate, int buffer, const char *fname, const char *tmpdir, double gamma, int nlayers, char *prevent, int full_detail, int low_detail, int min_detail);
+
 extern unsigned initial_x, initial_y;
 extern int geometry_scale;
