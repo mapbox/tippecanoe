9806db3c0a39765a78bb4bd532e2c92753e4bc02
Eric Fischer <enf@pobox.com>
2016-10-05 14:54:28 -0700

    Make UTF-8 checking into a unit test with Catch

---
+++ text.hpp
@@ -0,0 +1,3 @@
+#include <string>
+
+std::string check_utf8(std::string text);
---
+++ unit.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "catch/catch.hpp"
+#include "text.hpp"
+
+TEST_CASE("UTF-8 enforcement", "[utf8]") {
+	REQUIRE(check_utf8("") == std::string(""));
+	REQUIRE(check_utf8("hello world") == std::string(""));
+	REQUIRE(check_utf8("Καλημέρα κόσμε") == std::string(""));
+	REQUIRE(check_utf8("こんにちは 世界") == std::string(""));
+	REQUIRE(check_utf8("Hola m\xF3n") == std::string("\"Hola m\xF3n\" is not valid UTF-8 (0xF3 0x6E)"));
+}
