7514797c4c9d85eecc8b9c2d9d2bf4e54f0203bf
Eric Fischer <enf@pobox.com>
2016-12-06 17:25:23 -0800

    Add missing #includes

--- plugin.cpp
+++ plugin.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <vector>
 #include <string>
 #include <map>
 #include <pthread.h>
 #include <unistd.h>
 #include <cmath>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "mvt.hpp"
 #include "plugin.hpp"
 #include "projection.hpp"
