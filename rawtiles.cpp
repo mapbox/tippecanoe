9fb1208b79388b75e7764c5119db390862003620
Shan-Chun Kuo <joy961406@gmail.com>
2017-04-05 16:05:50 +0100

    Fix indentation for rawtiles.cpp

--- rawtiles.cpp
+++ rawtiles.cpp
@@ -7,14 +7,14 @@
 void write_raw_tile(char *outdir, int z, int tx, int ty, std::string pbf) {
 	mkdir(outdir, S_IRWXU | S_IRWXG | S_IRWXO);
 	std::string curdir(outdir);
-    std::string slash( "/" );
-    std::string newdir = curdir + slash + std::to_string(z);
+	std::string slash( "/" );
+	std::string newdir = curdir + slash + std::to_string(z);
 	mkdir(newdir.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
-   	newdir = newdir + "/" + std::to_string(tx);
-   	mkdir(newdir.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
-   	newdir = newdir + "/" + std::to_string(ty) + ".pbf";
-	
-    std::ofstream pbfFile (newdir, std::ios::out | std::ios::binary);
-    pbfFile.write (pbf.data(), pbf.size());
-    pbfFile.close();
+	newdir = newdir + "/" + std::to_string(tx);
+	mkdir(newdir.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+	newdir = newdir + "/" + std::to_string(ty) + ".pbf";
+
+	std::ofstream pbfFile (newdir, std::ios::out | std::ios::binary);
+	pbfFile.write (pbf.data(), pbf.size());
+	pbfFile.close();
 }
