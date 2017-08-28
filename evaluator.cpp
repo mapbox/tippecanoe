8d7f8af1d93bb7b1952f0d105f10d921b8e329af
Eric Fischer <enf@pobox.com>
2017-08-28 14:01:57 -0700

    Make indent

--- evaluator.cpp
+++ evaluator.cpp
@@ -8,7 +8,7 @@ int compare(mvt_value one, json_object *two, bool &fail) {
 	if (one.type == mvt_string) {
 		if (two->type != JSON_STRING) {
 			fail = true;
-			return false; // string vs non-string
+			return false;  // string vs non-string
 		}
 
 		return strcmp(one.string_value.c_str(), two->string);
@@ -17,7 +17,7 @@ int compare(mvt_value one, json_object *two, bool &fail) {
 	if (one.type == mvt_double || one.type == mvt_float || one.type == mvt_int || one.type == mvt_uint || one.type == mvt_sint) {
 		if (two->type != JSON_NUMBER) {
 			fail = true;
-			return false; // number vs non-number
+			return false;  // number vs non-number
 		}
 
 		double v;
@@ -48,7 +48,7 @@ int compare(mvt_value one, json_object *two, bool &fail) {
 	if (one.type == mvt_bool) {
 		if (two->type != JSON_TRUE && two->type != JSON_FALSE) {
 			fail = true;
-			return false; // bool vs non-bool
+			return false;  // bool vs non-bool
 		}
 
 		bool b = two->type = JSON_TRUE;
@@ -108,7 +108,7 @@ bool eval(std::map<std::string, mvt_value> const &feature, json_object *f) {
 
 		auto ff = feature.find(std::string(f->array[1]->string));
 		if (ff == feature.end()) {
-			return false; // not found: comparison is false
+			return false;  // not found: comparison is false
 		}
 
 		bool fail = false;
@@ -179,6 +179,6 @@ bool evaluate(std::map<std::string, mvt_value> const &feature, std::string const
 	if (f != NULL) {
 		return eval(feature, f);
 	} else {
-		return true; // no filter for this layer;
+		return true;  // no filter for this layer;
 	}
 }
