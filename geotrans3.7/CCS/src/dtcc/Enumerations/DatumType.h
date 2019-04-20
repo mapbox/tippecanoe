4c378a4fb34008d9ad7475d6e7cf2a6c7e86a258
mjj203 <40840725+mjj203@users.noreply.github.com>
2019-04-19 22:44:39 -0400

    Add files via upload

---
+++ geotrans3.7/CCS/src/dtcc/Enumerations/DatumType.h
@@ -0,0 +1,29 @@
+// CLASSIFICATION: UNCLASSIFIED
+
+#ifndef DatumType_H
+#define DatumType_H
+
+namespace MSP
+{
+  namespace CCS
+  {
+    class DatumType
+    {
+    public:
+
+      enum Enum
+      {
+        threeParamDatum,
+        sevenParamDatum,
+        wgs84Datum,
+        wgs72Datum
+      };
+    };
+  }
+}
+	
+#endif
+
+
+
+// CLASSIFICATION: UNCLASSIFIED
